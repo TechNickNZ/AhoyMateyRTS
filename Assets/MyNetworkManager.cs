cd0049e846bfd583bbb1be725df160bef2d32256
aRvBfan <rfi.band@gmail.com>
2018-10-24 16:44:24 +1300

    Created simply HUD element

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+    
+    public void MyStartHost()
+    {
+        StartHost();
+        Debug.Log("Starting Host at " + Time.timeSinceLevelLoad);
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Host started at " + Time.timeSinceLevelLoad);
+    }
+}
